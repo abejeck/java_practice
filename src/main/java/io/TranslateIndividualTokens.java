271d2926cf4e653a08c3aa175d21b6ae190f64c4
Allison Bejeck <allisonbejeck@Allison-Bejeck.local>
2024-07-18 11:49:50 -0400

    final errors fix 3

--- src/main/java/io/TranslateIndividualTokens.java
+++ src/main/java/io/TranslateIndividualTokens.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class TranslateIndividualTokens{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         Scanner scanner = null;
         double sum = 0;
 
