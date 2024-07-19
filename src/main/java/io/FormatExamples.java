e1df93fb0b9532e99f50819e6599931e26b66211
Allison Bejeck <allisonbejeck@Allison-Bejeck.local>
2024-07-19 15:21:40 -0400

    format Examples 1

---
+++ src/main/java/io/FormatExamples.java
@@ -0,0 +1,22 @@
+package main.java.io;
+
+public class FormatExamples{
+    public static void main(String[] args){
+        //print and println
+        int i = 2; 
+        double r = Math.sqrt(i);
+
+        System.out.print("The sqaure root of  " );
+        System.out.print(i);
+        System.out.print("is : ");
+        System.out.print(r);
+
+        i = 5; 
+        r = Math.sqrt(i);
+
+        System.out.println("The square root of " + i + "is: " + r);
+    }
+  
+
+
+}
