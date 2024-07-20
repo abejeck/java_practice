bc9fa06102d63fe0ec46747bfcc101c64abea4c4
Allison Bejeck <allisonbejeck@Allison-Bejeck.local>
2024-07-20 12:05:36 -0400

    rest of the format ex

--- src/main/java/io/FormatExamples.java
+++ src/main/java/io/FormatExamples.java
@@ -16,6 +16,30 @@ public class FormatExamples{
         r = Math.sqrt(i);
 
         System.out.println("The square root of " + i + " is: " + r + ".");
+
+        //format method
+        i = 93;
+        r = Math.sqrt(i);
+        /* %d -> formats integer value as a decimal value
+         * %f -> formats floating point value as a decimal value
+         * %n -> outputs a platform-specific line terminator.
+         */
+        System.out.format("The square root of %d is %f.%n", i, r);
+
+        //another format example:
+        /* % -> format specifier
+           1$ -> argument index
+           +0 -> flags : additional formatting options, the + specfies that the number
+           should always be formatted with a sign, the 0 specifies 0 is the
+           padding character
+           2 0 -> width: minium width of the formatted value (padded if necessary)
+           .10 -> precision: mathematical precision of the formatted value
+           f -> conversion
+         * 
+         */
+        System.out.format("%f, %1$+020.10f %n", Math.PI);
+
+
     }
   
 
